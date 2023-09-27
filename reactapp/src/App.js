3b36dc7e8d3c5d83a039a733709f3c2f9c967d1e
Iamneo <techadmin@iamneo.ai>
2023-09-27 08:14:53 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -21,14 +21,16 @@ function App() {
 
   return (
     <div className="App">
-       <form onSubmit={handleSubmit}>
-        <label>
-          Enter your name:
-          <input type="text" value={name} onChange={handleChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      <p>{greeting}</p>
+           <div className="contents">
+        <h3>Hey!!! Greeting</h3>
+        <form onSubmit={handleSubmit} data-testid="form">
+          <label data-testid="label">Enter name :</label>
+          <input type="text" value={name} onChange={handleChange}/>
+          <button type="submit" data-testid="buttonElement">Say Hello</button>
+          </form>
+          {greeting && <h2>{greeting}</h2>}
+    </div>
+
 
     </div>
   );
